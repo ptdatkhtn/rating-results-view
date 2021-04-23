e7904a6e11b11cdd1dca3b9899aa6d110c0a1058
ducphan <phan.duc@futuresplatform.com>
2021-04-23 13:22:46 +0300

    fix (rating-result): fixed minor bugs

--- src/components/RatingResult/RatingResult.js
+++ src/components/RatingResult/RatingResult.js
@@ -87,8 +87,8 @@ const Rating = ({ phenomenon, radar, isRatingX }) => {
         </RatingHeader>
         <RatingItem>
           <RatingSliderScale>
-              <SliderScaleMin>1</SliderScaleMin>
-              <SliderScaleMax>100</SliderScaleMax>
+              <SliderScaleMin>{isRatingX? radar.axisXMin : radar.axisYMin}</SliderScaleMin>
+              <SliderScaleMax>{isRatingX? radar.axisXMax : radar.axisYMax}</SliderScaleMax>
             </RatingSliderScale>
           <RatingSlider
             className="inactive"
