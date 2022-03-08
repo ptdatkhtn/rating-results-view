3ad74ea7b85a24a43323f901c61df36b8e8ce3db
Phan <phan.duc@futuresplatform.com>
2022-03-08 02:22:07 +0200

    Closed content-card in fullscreen mode when user clicks exit fullscreen mode button

--- src/components/FourfoldTable/index.js
+++ src/components/FourfoldTable/index.js
@@ -75,6 +75,7 @@ const App = ({
   }
   const handleExitFullScreenMode = () => {
     try {
+      closePopupLeftfs()
       if (document.exitFullscreen) {
           document.exitFullscreen();
       } else if (document.webkitExitFullscreen) { /* Safari */
