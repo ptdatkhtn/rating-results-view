bb34b4a702999a0c93015a9c5c4383060970ea2f
Phan <phan.duc@futuresplatform.com>
2022-04-09 14:45:29 +0300

    Fixed can not scroll down

--- src/styles.js
+++ src/styles.js
@@ -1,27 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const RatingResultsStyles = createGlobalStyle` 
-   html, body {
-      overflow: hidden !important;
-      font-family: 'L10' !important;
-      background-color: rgba(0,0,0,0) !important;
-  }
-  
-  .radar-widget-title {
-    font-size: 18px;
-  }
-  
-  .fp-wysiwyg .ql-editor p, 
-  .fp-wysiwyg .ql-editor h1,
-  .fp-wysiwyg .ql-editor h2,
-  .fp-wysiwyg .ql-editor h3,
-  .fp-wysiwyg .ql-editor h4,
-  .fp-wysiwyg .ql-editor h5,
-  .fp-wysiwyg .ql-editor h6,
-  .fp-wysiwyg .ql-editor iframe,
-  .fp-wysiwyg .ql-editor img {
-    margin-bottom: 20px;
-  }
+   
 
   .af-custom-fullscreen_exit:before {
     content: "";
